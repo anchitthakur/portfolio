ba9d079b5be18b1e60a4dbae1b709ab9c6c76071
Anchit Thakur <anchit.thakur11@gmail.com>
2020-06-15 02:11:18 +0530

    Progress on projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,61 +2,54 @@ import Layout from "../components/layout"
 import React from "react"
 import ProjectSvg from "../images/assets/projects.svg"
 import "../components/projects.css"
-import Moowe from "../images/moowe.png"
-import News from "../images/News.png"
+import NextJs from "../images/assets/nextjs.svg"
+import NodeSvg from "../images/assets/nodejs.svg"
+import MongoSvg from "../images/assets/mongodb.svg"
+import BootstrapSvg from "../images/assets/bootstrap.svg"
+import ExpressSvg from "../images/assets/expressjs.svg"
+import ReactSvg from "../images/assets/reactJs.svg"
+// import News from "../images/News.png"
 
 export default () => {
+  console.log(ReactSvg)
   return (
     <>
       <div className="flex-container">
         <ProjectSvg/>
         <h1> Projects </h1>
       </div>
-      <div className="flex-container">
-        <ul>
-          <li>
-            <a target="_blank" href='https://moowe.herokuapp.com'>Moowe</a>
-            <ul>
-              <li>This website is a catalogue of Movies and TV shows made with a unified search approach.</li>
-              <li>Tech Stack:</li>
-              <ul>
-                <li>React</li>
-                <li>Next.js</li>
-                <li>Bootstrap</li>
-                <li>Node.js</li>
-                <li>Express.js</li>
-                <li>MongoDB</li>
-              </ul>
-            </ul>
-          </li>
-          <li>
-            <a target="_blank" href='https://nextjs.anchitthakur11.now.sh/ApiPages'>News</a>
-            <ul>
-              <li>This is an isomorphic website made using next.js that I made during my time as an intern in the PWA
-                team at IndiaMART.
-              </li>
-              <li>Tech Stack:</li>
-              <ul>
-                <li>React</li>
-                <li>Next.js</li>
-                <li>Bootstrap</li>
-              </ul>
-            </ul>
-          </li>
-          <li>
-            <a target="_blank" href='https://holidaycalender.anchitthakur11.now.sh/'>Holiday Calender</a>
-            <ul>
-              <li>I made this website for a freelance job that I was hired for. It's a simple react application which
-                tells you about upcoming and past holidays and whether it's a holiday today.
-              </li>
-              <li>Tech Stack:</li>
-              <ul>
-                <li>React</li>
-                <li>Reactstrap</li>
-              </ul>
-            </ul>
-          </li>
-        </ul>
+      <div className="flex-container" style={{padding:'20px',height:'400px', overflow:'auto'}}>
+        <div className="card">
+          <a target="_blank" href='https://moowe.herokuapp.com'>Moowe</a><br/>
+          This website is a catalogue of Movies and TV shows made with a unified search approach.
+          <div className="flex-container">
+            <NextJs/>
+            <BootstrapSvg/>
+            <ExpressSvg/>
+            <NodeSvg/>
+            <MongoSvg/>
+          </div>
+        </div>
+        <div className="card">
+          <a target="_blank" href='https://nextjs.anchitthakur11.now.sh/ApiPages'>News</a><br/>
+          This is an isomorphic website made using next.js that I made during my time as an intern in the PWA
+          team at IndiaMART.
+
+          <div className="flex-container" style={{marginTop:'10px'}}>
+            <NextJs/>
+            <BootstrapSvg/>
+          </div>
+        </div>
+        <div className="card">
+          <a target="_blank" href='https://holidaycalender.anchitthakur11.now.sh/'>Holiday Calender</a><br/>
+          I made this website for a freelance job that I was hired for. It's a simple react application which
+          tells you about upcoming and past holidays and whether it's a holiday today.
+
+          <div className="flex-container" style={{marginTop:'10px'}}>
+            <ReactSvg/>
+            <BootstrapSvg/>
+          </div>
+        </div>
       </div>
     </>
   )
